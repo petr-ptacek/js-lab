91210f5a91ff1f131079f60f78938326a237cebb
Petr Ptacek <petr.ptacek@olc.cz>
2026-01-09 12:50:03 +0100

    Init packages/core

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es"],
+    },
+    rolldownOptions: {
+      external: ["vue"],
+    },
+  },
+});

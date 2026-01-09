e3ac063158072c74fe92ef8a254ac93d7bc445f6
Petr Ptacek <petr.ptacek@olc.cz>
2026-01-09 15:12:43 +0100

    updates

--- packages/core/src/composables/useSum.ts
+++ packages/core/src/composables/useSum.ts
@@ -2,9 +2,9 @@ import { computed, type MaybeRef, toValue } from "vue";
 import { sum } from "@/utils";
 
 export function useSum(a: MaybeRef<number>, b: MaybeRef<number>) {
-  const result = computed(() => {
+  const sumared = computed(() => {
     return sum(toValue(a), toValue(b));
   });
 
-  return { result, sum };
+  return { sumared };
 }

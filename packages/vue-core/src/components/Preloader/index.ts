18edb1e5c1c580dfa8c370eb36e4e4aafc35f67a
Petr Ptacek <petr.ptacek@olc.cz>
2026-02-13 14:52:02 +0100

    refactor(vue-core): types of preloader

--- packages/vue-core/src/components/Preloader/index.ts
+++ packages/vue-core/src/components/Preloader/index.ts
@@ -1,8 +1,9 @@
 import "./styles.css";
 
 export type {
-  Props as UiPreloaderProps,
-  Slots as UiPreloaderSlots,
+  Size as PreloaderSize,
+  Props as PreloaderProps,
+  Slots as PreloaderSlots,
 } from "./types.ts";
 
 export { default as UiPreloader } from "./Preloader.vue";
